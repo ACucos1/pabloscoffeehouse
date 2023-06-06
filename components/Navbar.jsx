61d527367db6c2d7e97ac028e6aa71ef846d6eb2
Alex <alexcucos1@gmail.com>
2023-06-06 18:04:09 -0400

    add selected tab underline in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.scss";
 
 export const Navbar = ({ navLinks }) => {
   const router = useRouter();
-  const handleScroll = (e) => {
+  const handleClick = (e) => {
     const href = e.currentTarget.href;
     if (href.match(/\#/) && router.asPath !== "/") e.preventDefault();
 
@@ -22,19 +22,39 @@ export const Navbar = ({ navLinks }) => {
     <nav className={`${styles.Nav}`}>
       <div className={styles.NavInner}>
         <div className={`${styles.NavGroup} ${styles.NavGroup1}`}>
-          <Link href={navLinks.navLinks[0].link} onClick={handleScroll}>
+          <Link
+            href={navLinks.navLinks[0].link}
+            onClick={handleClick}
+            className={
+              navLinks.navLinks[0].link == router.asPath && styles.Selected
+            }>
             {navLinks.navLinks[0].linkText}
           </Link>
-          <Link href={navLinks.navLinks[1].link} onClick={handleScroll}>
+          <Link
+            href={navLinks.navLinks[1].link}
+            onClick={handleClick}
+            className={
+              navLinks.navLinks[1].link == router.asPath && styles.Selected
+            }>
             {navLinks.navLinks[1].linkText}
           </Link>
         </div>
         <img src='/LogoIcon4.png' alt='logo' className={`${styles.Logo}`} />
         <div className={`${styles.NavGroup} ${styles.NavGroup2}`}>
-          <Link href={navLinks.navLinks[2].link} onClick={handleScroll}>
+          <Link
+            href={navLinks.navLinks[2].link}
+            onClick={handleClick}
+            className={
+              navLinks.navLinks[2].link == router.asPath && styles.Selected
+            }>
             {navLinks.navLinks[2].linkText}
           </Link>
-          <Link href={navLinks.navLinks[3].link} onClick={handleScroll}>
+          <Link
+            href={navLinks.navLinks[3].link}
+            onClick={handleClick}
+            className={
+              navLinks.navLinks[3].link == router.asPath && styles.Selected
+            }>
             {navLinks.navLinks[3].linkText}
           </Link>
         </div>
