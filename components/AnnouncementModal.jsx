7be8718413d707241120e1e8b98e62cd88f13fda
Alex <alexcucos1@gmail.com>
2024-01-15 17:06:45 -0500

    Add announcement modal to index

---
+++ components/AnnouncementModal.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/AnnouncementModal.module.scss";
+
+export default function AnnouncementModal() {
+  const [open, setOpen] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    const form = e.target;
+    if (!form.checkValidity()) return;
+
+    const formData = new FormData(form);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    });
+    e.preventDefault();
+  };
+
+  const handleModalClose = () => {
+    setOpen(false);
+    localStorage.setItem("modalClosed", "true");
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem("modalClosed")) {
+      setOpen(true);
+    }
+  }, [open, setOpen]);
+
+  if (open) {
+    return (
+      <div className={styles.ModalBg}>
+        <div className={styles.Modal}>
+          <button onClick={handleModalClose} className={styles.CloseBtn}>
+            &#x2715;
+          </button>
+          <h1>We're Moving!</h1>
+          <p>
+            We are thrilled to announce that Pablo's Coffee House is expanding
+            to a <strong>full-size unit</strong> inside the Shops at Don Mills
+            mall!
+          </p>
+          <p>
+            Our new location is situated at
+            <strong> 32 Clock Tower Road, North York, M3C0G5</strong>. We're
+            currently under construction, but we will resume operation shortly.
+          </p>
+          <p>
+            <i>Thank you</i> for your continued support and loyalty. We are
+            committed to providing you with the same quality coffee, pastries,
+            food, and service that you have come to expect from us.
+          </p>
+          <p>
+            We will keep you updated on our progress and look forward to
+            welcoming you to our new location soon!
+          </p>
+
+          <h3>Join our newsletter to stay up to date!</h3>
+          <form
+            action='/'
+            method='POST'
+            data-netlify='true'
+            name='newsletter-form-modal'
+            onSubmit={handleFormSubmit}>
+            <input
+              name='email'
+              type='email'
+              required
+              placeholder='Enter your email here'
+            />
+            <input type='submit' />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,7 @@ import client from "../sanity/client";
 import styles from "../styles/Home.module.scss";
 import { useRouter } from "next/router";
 import { gsap } from "gsap";
+import AnnouncementModal from "../components/AnnouncementModal";
 
 const GOOGLE_MAPS_API_KEY = "AIzaSyBY-wvD3hJfZXZ2w5HaH4VtePd_k6Mf2ow";
 
@@ -68,7 +69,7 @@ Sample our exquisite blends, fine pastries, and refreshing iced drinks.'
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Navbar navLinks={navLinks} />
-
+      <AnnouncementModal />
       <section className={styles.Home} id='Home'>
         <div className={styles.Hero}>
           <div className={styles.HeroLogo}>
